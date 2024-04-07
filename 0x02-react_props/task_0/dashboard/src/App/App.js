e98a4850b1b7da4eb4903ef6ced3f2da17dcf2b4
jassyio <joejassyio@gmail.com>
2024-04-07 16:37:20 +0300

    task_0

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
+import Footer from "../../../../src/Footer/Footer";
 import "./App.css";
 
 function App() {
